126907c29ff346f97226feb0e8248806ed2c2e83
Andy Faulkner <andrewfaulkner80@gmail.com>
2016-07-06 16:25:35 +0100

    y

---
+++ MVCDemo/MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult About(string myName = "", int myNumber = 1)
+        {
+            ViewBag.Message2 = "Hello" + myName + " you typed in the number " + myNumber;
+            return View();
+        }
+
+        //getting a parameter via routing
+        public ActionResult Contact(int id = 1)
+        {
+            ViewBag.AnotherMessage = "You have just enteredd the number " + id;
+            return View();
+        }
+    }
+}
---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForms()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForms(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
